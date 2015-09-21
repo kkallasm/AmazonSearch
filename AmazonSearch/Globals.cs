7b30318c4f6b36e13ed48b5acf724597825ab6db
kkallasm <kaspar68@gmail.com>
2015-09-21 09:09:02 +0300

    fixes

--- AmazonSearch/Globals.cs
+++ AmazonSearch/Globals.cs
@@ -7,9 +7,10 @@ namespace AmazonSearch
 {
     public class Globals
     {
-        public const string ACCESS_KEY_ID = "AKIAIHFTQLGAP7YUYGHQ";
-        public const string SECRET_KEY = "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";
+        public const string ACCESS_KEY_ID = "<access_key>";
+        public const string SECRET_KEY = "<secret_key>";
         public const CurrencyConvertor.Currency BASE_CURRENCY = CurrencyConvertor.Currency.USD;
         public const int ITEMS_PER_PAGE = 13;
+        public const int AMAZON_ITEMS_PER_PAGE = 10;
     }
 }
--- AmazonSearch/Models/ProductContainer.cs
+++ AmazonSearch/Models/ProductContainer.cs
@@ -32,77 +32,67 @@ namespace AmazonSearch.Models
             set { pageCount = value; }
         }
 
-        public static ProductContainer getProducts(String search, int pageNum)
+        protected static ProductContainer _getProducts(String search, int pageNum)
         {
             List<Product> products = new List<Product>();
-
             int pageCount = 0;
             int totalResults = 0;
 
-            //for (int pNum = pageNum; pNum <= pageNum + 1; pNum++)
-            //{
-            //    pageNum = pNum;
+            //prepare an ItemSearch request
+            ItemSearchRequest request = new ItemSearchRequest();
+            request.SearchIndex = "All";
+            request.Keywords = search;
+            request.ResponseGroup = new string[] { "ItemAttributes", "Small", "Images" };
+            request.ItemPage = pageNum.ToString();
 
-
-                //prepare an ItemSearch request
-                ItemSearchRequest request = new ItemSearchRequest();
-                request.SearchIndex = "All";
-                request.Keywords = search;
-                request.ResponseGroup = new string[] { "ItemAttributes", "Small", "Images" };
-                request.ItemPage = pageNum.ToString();
-
-                ItemSearchResponse res = AmazonService.search(request);
-                if (res.Items[0].Item != null)
+            ItemSearchResponse res = AmazonService.search(request);
+            if (res.Items[0].Item != null)
+            {
+                foreach (var item in res.Items[0].Item)
                 {
-                    foreach (var item in res.Items[0].Item)
+                    if (item == null) continue;
+                    string url = item.DetailPageURL;
+
+                    if (url.EndsWith("null"))
+                        url = url.Remove(url.Length - 4);
+
+                    int price = -1;
+                    string formattedPrice = "";
+                    if (item.ItemAttributes.ListPrice != null)
                     {
-                        if (item == null) continue;
-                        string url = item.DetailPageURL;
-
-                        if (url.EndsWith("null"))
-                            url = url.Remove(url.Length - 4);
-
-                        int price = -1;
-                        string formattedPrice = "";
-                        if (item.ItemAttributes.ListPrice != null)
-                        {
-                            price = Convert.ToInt32(item.ItemAttributes.ListPrice.Amount);
-                            formattedPrice = item.ItemAttributes.ListPrice.FormattedPrice;
-                        }
-
-                        Product product = new Product
-                        {
-                            Title = item.ItemAttributes.Title,
-                            Link = url,
-                            Image = item.MediumImage,
-                            Price = price,
-                            FormattedPrice = formattedPrice
-                        };
-
-                        products.Add(product);
-
-                    }
-                }
-
-
-                if (products.Count > 0)
-                {
-                    totalResults = Convert.ToInt32(res.Items[0].TotalResults);
-                    if (totalResults > 0)
-                    {
-                        if (totalResults <= Globals.ITEMS_PER_PAGE)
-                            pageCount = 1;
-                        else
-                        {
-                            pageCount = totalResults / Globals.ITEMS_PER_PAGE;
-                            if (totalResults % Globals.ITEMS_PER_PAGE != 0)
-                                pageCount++;
-                        }
+                        price = Convert.ToInt32(item.ItemAttributes.ListPrice.Amount);
+                        formattedPrice = item.ItemAttributes.ListPrice.FormattedPrice;
                     }
 
+                    Product product = new Product
+                    {
+                        Title = item.ItemAttributes.Title,
+                        Link = url,
+                        Image = item.MediumImage,
+                        Price = price,
+                        FormattedPrice = formattedPrice
+                    };
+
+                    products.Add(product);
+
                 }
-                //Thread.Sleep(10000);
-            //}
+            }
+
+            if (products.Count > 0)
+            {
+                totalResults = Convert.ToInt32(res.Items[0].TotalResults);
+                if (totalResults > 0)
+                {
+                    if (totalResults <= Globals.ITEMS_PER_PAGE)
+                        pageCount = 1;
+                    else
+                    {
+                        pageCount = totalResults / Globals.ITEMS_PER_PAGE;
+                        if (totalResults % Globals.ITEMS_PER_PAGE != 0)
+                            pageCount++;
+                    }
+                }
+            }
 
             return new ProductContainer
             {
@@ -110,7 +100,42 @@ namespace AmazonSearch.Models
                 pageCount = pageCount,
                 resultCount = totalResults
             };
+        }
 
+        public static ProductContainer getProducts(String search, int pageNum)
+        {
+            List<Product> products = new List<Product>();
+            int pageCount = 0;
+            int totalResults = 0;
+
+            int start = (pageNum - 1) * Globals.ITEMS_PER_PAGE;       
+            int firstElem = start % Globals.AMAZON_ITEMS_PER_PAGE; 
+            int lastElem = firstElem + Globals.ITEMS_PER_PAGE; 
+            int currentPage = pageNum;
+
+            ProductContainer tmpProducts = new ProductContainer();
+            for (int i = firstElem; i < lastElem; i++)
+            {
+                if (i == firstElem || i == Globals.AMAZON_ITEMS_PER_PAGE)
+                {
+                    tmpProducts = _getProducts(search, currentPage);
+                    pageCount = tmpProducts.pageCount;
+                    totalResults = tmpProducts.resultCount;
+                    currentPage++;
+                }
+
+                if (i % Globals.AMAZON_ITEMS_PER_PAGE >= tmpProducts.products.Count)
+                    break;
+
+                products.Add(tmpProducts.products[i % Globals.AMAZON_ITEMS_PER_PAGE]);
+            }
+
+            return new ProductContainer
+            {
+                products = products,
+                pageCount = pageCount,
+                resultCount = totalResults
+            };
         }
     }
 }
